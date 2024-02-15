f0d9aef29e2ee500935e14840a59d26359d585eb
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-15 22:12:59 +0200

    done with tasks1,2,3

--- Backend/express.js/index.js
+++ Backend/express.js/index.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-
+const port = 3000;
 app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-})
+  console.log(`Server running on port ${port}`);
+});
+
+//-----get--------------------------------------
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1>");
+});
+
+
