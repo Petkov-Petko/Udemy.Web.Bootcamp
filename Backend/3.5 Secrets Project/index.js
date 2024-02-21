aa35ff3f9c0c48421809e3d4558215fe7a6059b6
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-21 21:57:09 +0200

    Done with the project. Everything works fine.

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,48 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+let authorized = false;
+
+function checkPassword(req, res, next) {
+    const pass = req.body["password"];
+    if (pass === "ILoveProgramming") {
+        authorized = true;
+    }
+    next();
+}
+
+app.use(checkPassword)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post('/check/', (req, res) => {
+    if(authorized){
+        res.sendFile(__dirname + "/public/secret.html");
+    }else{
+        res.redirect("/")
+        authorized = false;
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
