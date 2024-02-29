2ec117a3ae2cdc5c6d95081c440d3e22aa6dcd8e
Petko Petkov <petkozlatilov1234@gmail.com>
2024-02-29 22:51:36 +0200

    Note.jsx

--- React/my-app/src/components/Note.jsx
+++ React/my-app/src/components/Note.jsx
@@ -3,12 +3,11 @@ import { notes } from "../notes";
 export function Note() {
   return notes.map((el) => {
     return (
-      <div className="note">
+      <div className="note" key={el.key}>   {/* We always need a key to don't get and error in the browser. */}
         <h1>{el.title}</h1>
         <p>{el.content}</p>
       </div>
     );
   });
 }
-
 
